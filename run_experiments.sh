b863e83f164a7f4b7d4d8253582060aad35763e3
Hector Martinez <hectorxm@mit.edu>
2026-01-31 13:54:26 -0600

    correct non-extreme-ws code

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,4 +1,4 @@
-export SAVE_DIR="./visualizations/comm=20-rep=5-changed-u-target"
+export SAVE_DIR="./visualizations/comm=20-rep=5-high-discrete-non-extreme-ws"
 python3 new_models/plot_results.py --punishment none --save-dir $SAVE_DIR"/harsh_and_none" --device gpu
 
 python3 new_models/plot_results.py --punishment mild --save-dir $SAVE_DIR"/harsh_and_mild" --device gpu
