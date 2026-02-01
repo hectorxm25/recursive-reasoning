d7d19512834791f2e35dcad2b6eb96a0ed41161e
Hector X Martinez <hectorxm@align-3.csail.mit.edu>
2026-02-01 13:53:13 -0500

    adding final plots results

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,12 +1,14 @@
-export SAVE_DIR="./visualizations/w-1/changed-u-target-extreme-ws"
-python3 new_models/plot_results.py --punishment none --save-dir $SAVE_DIR"/harsh_and_none" --device gpu
+export SAVE_DIR="./visualizations/final_plots/run_2_rep_only"
+# python3 new_models/plot_results.py --punishment none --save-dir $SAVE_DIR"/harsh_and_none" --device gpu
 
-python3 new_models/plot_results.py --punishment mild --save-dir $SAVE_DIR"/harsh_and_mild" --device gpu
+# python3 new_models/plot_results.py --punishment mild --save-dir $SAVE_DIR"/harsh_and_mild" --device gpu
 
-python3 new_models/plot_results.py --punishment total-punishment --save-dir $SAVE_DIR"/total-punishment" --device gpu
+# python3 new_models/plot_results.py --punishment total-punishment --save-dir $SAVE_DIR"/total-punishment" --device gpu
 
-python3 new_models/plot_results.py --belief-update --save-dir $SAVE_DIR"/belief-update" --device gpu
+# python3 new_models/plot_results.py --belief-update --save-dir $SAVE_DIR"/belief-update" --device gpu
 
-python3 new_models/plot_results.py --utility-mode --save-dir $SAVE_DIR"/utility-all" --device gpu
+# python3 new_models/plot_results.py --utility-mode --save-dir $SAVE_DIR"/utility-all" --device gpu
 
-python3 new_models/plot_results.py --w1 --save-dir $SAVE_DIR"/w1" --device gpu
+# python3 new_models/plot_results.py --w1 --save-dir $SAVE_DIR"/w1" --device gpu
+
+python3 new_models/plot_results.py --final-plots --save-dir $SAVE_DIR --device gpu
